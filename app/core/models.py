7c7f8e6bbfd308b83e93e7a6b853f71ed9442533
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2019-03-21 15:39:34 +0900

    little change

--- app/core/models.py
+++ app/core/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
         if not email:
